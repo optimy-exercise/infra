5f05e4905765fd956baf274274467b6a326442b2
Giorgos Tsoulos <giorgos.tsoulos@fortigo.com>
2024-07-25 17:30:39 +0200

    Add grafana config and iam role with rights

--- server-init/main.tf
+++ server-init/main.tf
@@ -11,11 +11,78 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_caller_identity" "current" {}
+
+
+resource "aws_iam_role" "grafana_role" {
+  name = "grafana-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          AWS = "*"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "grafana_policy" {
+  name        = "grafana-policy"
+  description = "Policy for Grafana to access CloudWatch"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "sts:AssumeRole",
+          "cloudwatch:*",
+          "logs:*"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "grafana_assume_role_policy" {
+  name   = "grafana-assume-role-policy"
+  role   = aws_iam_role.grafana_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "sts:AssumeRole",
+        Resource = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/grafana-role"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "grafana_role_attachment" {
+  role       = aws_iam_role.grafana_role.name
+  policy_arn = aws_iam_policy.grafana_policy.arn
+}
+
+resource "aws_iam_instance_profile" "grafana_instance_profile" {
+  name = "grafana-instance-profile"
+  role = aws_iam_role.grafana_role.name
+}
+
 resource "aws_instance" "php_app" {
   ami           = "ami-071878317c449ae48"
   instance_type = var.instance_type
   key_name      = var.key_name
   subnet_id     = element(var.subnet_ids, 0)
+  iam_instance_profile   = aws_iam_instance_profile.grafana_instance_profile.name
 
   tags = {
     Name    = "php-app"
@@ -64,6 +131,13 @@ resource "aws_security_group" "php_app_sg" {
   ingress {
     from_port   = 3306
     to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
